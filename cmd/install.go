f89943fe1483d8c69c0eda8553de415c356d8968
Chris Jones <chris@Chriss-MacBook-Air.local>
2022-06-06 10:20:42 -0700

    add console out viewport to install tui

--- cmd/install.go
+++ cmd/install.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
@@ -13,7 +15,13 @@ import (
 )
 
 type installModel struct {
-	inst *install.Install
+	inst              *install.Install
+	viewport          *viewport.Model
+	height            int
+	width             int
+	groupCount        int
+	commandCount      int
+	viewportMaxHeight int
 }
 
 type installMsg string
@@ -23,13 +31,26 @@ const (
 	completeMsg installMsg = "complete"
 )
 
+var (
+	groupLineStyle = lipgloss.NewStyle().
+			PaddingTop(1)
+
+	commandLineStyle = lipgloss.NewStyle().
+				PaddingLeft(3)
+
+	viewportStyle = lipgloss.NewStyle().
+			MarginLeft(5).
+			PaddingLeft(2).
+			Border(lipgloss.NormalBorder(), false, false, false, true)
+)
+
 var installCmd = &cobra.Command{
 	Use:   "install [GROUPS]",
 	Short: "run configured install scripts",
 	Long:  "run configured install scripts",
 	Args:  cobra.ArbitraryArgs,
 	Run: func(_ *cobra.Command, args []string) {
-		program := tea.NewProgram(&installModel{})
+		program := tea.NewProgram(initModel())
 
 		go func() {
 			config := viper.Get("install")
@@ -45,7 +66,9 @@ var installCmd = &cobra.Command{
 				}).
 				Install()
 			if err != nil {
-				panic(err)
+				program.Send(progressMsg)
+				program.Send(completeMsg)
+				os.Exit(1)
 			}
 
 			program.Send(progressMsg)
@@ -62,6 +85,14 @@ func init() {
 	rootCmd.AddCommand(installCmd)
 }
 
+func initModel() tea.Model {
+	viewport := viewport.New(100, 5)
+
+	return &installModel{
+		viewport: &viewport,
+	}
+}
+
 func (m *installModel) Init() tea.Cmd {
 	return nil
 }
@@ -76,70 +107,157 @@ func (m *installModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if msg == completeMsg {
 			return m, tea.Quit
 		}
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
 	case *install.Install:
 		m.inst = msg
+		m.updateCounts()
 		return m, nil
 	}
 
 	return m, nil
 }
 
+func (m *installModel) updateCounts() {
+	m.groupCount = 0
+	m.commandCount = 0
+	for i := range m.inst.Groups {
+		group := &m.inst.Groups[i]
+		if group.Status == install.Skipped {
+			continue
+		}
+		m.groupCount++
+		for i := range group.Commands {
+			command := &group.Commands[i]
+			if command.Status == install.Skipped {
+				continue
+			}
+			m.commandCount++
+		}
+	}
+
+	groupHeight := lipgloss.Height(m.groupLineView(&install.Group{Name: "foo"}))
+	groupsHeight := m.groupCount * groupHeight
+	commandHeight := lipgloss.Height(
+		m.commandLineView(&install.Command{Command: "foo"}),
+	)
+	commandsHeight := m.commandCount * commandHeight
+
+	viewportMaxHeight := m.height - groupsHeight - commandsHeight - 2
+
+	if viewportMaxHeight > 0 {
+		m.viewport.Height = viewportMaxHeight
+		m.viewportMaxHeight = viewportMaxHeight
+	}
+}
+
 func (m *installModel) View() string {
 	if m.inst == nil {
 		return ""
 	}
-	return groupsView(m.inst.Groups)
+	return m.groupsView(m.inst.Groups)
 }
 
-func groupsView(groups []install.Group) string {
+func (m *installModel) groupsView(groups []install.Group) string {
 	sb := &strings.Builder{}
 	for i := range groups {
 		group := &groups[i]
-		groupView(sb, group)
+		m.groupView(sb, group)
 	}
 	return sb.String()
 }
 
-func groupView(sb *strings.Builder, group *install.Group) string {
+func (m *installModel) groupView(
+	sb *strings.Builder,
+	group *install.Group,
+) string {
 	if group.Status == install.Skipped {
 		return ""
 	}
 
-	fmt.Fprintf(sb, "\n%s\n", group.Name)
-	commandsView(sb, group.Commands)
+	fmt.Fprintf(sb, "%s\n", m.groupLineView(group))
+	m.commandsView(sb, group.Commands)
 
 	return sb.String()
 }
 
-func commandsView(sb *strings.Builder, commands []install.Command) {
+func (m *installModel) groupLineView(group *install.Group) string {
+	return groupLineStyle.Render(group.Name)
+}
+
+func (m *installModel) commandsView(
+	sb *strings.Builder,
+	commands []install.Command,
+) {
 	for i := range commands {
 		command := &commands[i]
-		commandView(sb, command)
+		m.commandView(sb, command)
 	}
 }
 
-func commandView(sb *strings.Builder, command *install.Command) {
+func (m *installModel) commandView(
+	sb *strings.Builder,
+	command *install.Command,
+) {
 	if command.Status == install.Skipped {
 		return
 	}
 
 	fmt.Fprintf(
 		sb,
-		"  [%s] %s\n",
-		command.Status,
-		truncCommand(command.Command),
+		"%s\n",
+		m.commandLineView(command),
 	)
 
 	if command.Status != install.Error && command.Status != install.Running {
 		return
 	}
 
+	outLineCount := getOutLineCount(command.Out)
+	if outLineCount < m.viewportMaxHeight {
+		m.viewport.Height = outLineCount
+	} else {
+		m.viewport.Height = m.viewportMaxHeight
+	}
+
+	m.viewport.SetContent(string(getContentForViewPort(command.Out)))
+	m.viewport.GotoBottom()
+
 	fmt.Fprintf(
 		sb,
 		"%s\n",
-		lipgloss.NewStyle().
-			MarginLeft(6).
-			Render(string(command.Out)),
+		viewportStyle.Render(m.viewport.View()),
+	)
+}
+
+func getContentForViewPort(b []byte) []byte {
+	bLen := len(b)
+
+	if bLen > 0 && b[bLen-1] == '\n' {
+		return b[:bLen-1]
+	}
+
+	return b
+}
+
+func getOutLineCount(out []byte) int {
+	count := 0
+	for _, b := range out {
+		if b == '\n' {
+			count++
+		}
+	}
+	return count
+}
+
+func (m *installModel) commandLineView(command *install.Command) string {
+	return commandLineStyle.Render(
+		fmt.Sprintf(
+			"[%s] %s",
+			command.Status,
+			truncCommand(command.Command),
+		),
 	)
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yo3jones/yconfig
 go 1.18
 
 require (
+	github.com/charmbracelet/bubbles v0.11.0
 	github.com/charmbracelet/bubbletea v0.21.0
 	github.com/charmbracelet/lipgloss v0.5.0
 	github.com/spf13/cobra v1.4.0
