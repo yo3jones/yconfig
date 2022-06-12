3004296c42decdb27c0d2a12d47ae95387991492
Chris Jones <chris@Chriss-MacBook-Air.local>
2022-06-12 14:56:10 -0700

    add exec functionality to setup

--- setup/filter.go
+++ setup/filter.go
@@ -30,6 +30,11 @@ type filterer struct {
 	initialized bool
 }
 
+const (
+	restrictive    = true
+	nonRestrictive = false
+)
+
 func NewFilterer() Filterer {
 	return &filterer{initialized: false}
 }
@@ -43,7 +48,8 @@ func (f *filterer) Tags(tags map[string]bool) Filterer {
 func (f *filterer) FilterSystemScripts(
 	scripts []*Script,
 ) (systemScript *Script, err error) {
-	if foundScript, found, err := filter(f, scripts); err != nil {
+	foundScript, found, err := filter(f, scripts, nonRestrictive)
+	if err != nil {
 		return nil, err
 	} else if !found {
 		return nil, fmt.Errorf("no system script found")
@@ -55,7 +61,12 @@ func (f *filterer) FilterSystemScripts(
 func (f *filterer) FilterSystemPackageManagers(
 	packageManagers []*PackageManager,
 ) (systemPackageManager *PackageManager, err error) {
-	if foundPackageManager, found, err := filter(f, packageManagers); err != nil {
+	foundPackageManager, found, err := filter(
+		f,
+		packageManagers,
+		nonRestrictive,
+	)
+	if err != nil {
 		return nil, err
 	} else if !found {
 		return nil, fmt.Errorf("no system package manager found")
@@ -68,7 +79,8 @@ func (f *filterer) FilterValues(setup *Setup) (values []Value, err error) {
 	values = make([]Value, 0, len(setup.Entries))
 
 	for _, entry := range setup.Entries {
-		if foundValue, found, err := filter(f, entry.Values); err != nil {
+		foundValue, found, err := filter(f, entry.Values, restrictive)
+		if err != nil {
 			return nil, err
 		} else if found {
 			values = append(values, foundValue)
@@ -87,7 +99,8 @@ func (f *filterer) initialize() (err error) {
 		return err
 	}
 
-	if f.runtimeArch, err = archtypes.ArchFromString(runtime.GOARCH); err != nil {
+	f.runtimeArch, err = archtypes.ArchFromString(runtime.GOARCH)
+	if err != nil {
 		return err
 	}
 
@@ -97,6 +110,7 @@ func (f *filterer) initialize() (err error) {
 func filter[T Filterable, E ~[]T](
 	f *filterer,
 	items E,
+	restrictive bool,
 ) (foundItem T, found bool, err error) {
 	if err = f.initialize(); err != nil {
 		return foundItem, false, err
@@ -108,39 +122,23 @@ func filter[T Filterable, E ~[]T](
 	for i := range items {
 		item := items[i]
 
-		requiredTagsMatch := containsAllTags(
-			f.runtimeTags,
-			item.GetRequiredTags(),
+		keepCompatible, keepTag, keepRequiredTag := shouldKeep(
+			f,
+			item,
+			restrictive,
 		)
 
-		if !requiredTagsMatch {
-			continue
+		if keepCompatible {
+			compatibleItems = append(compatibleItems, item)
 		}
 
-		if item.GetOs() != ostypes.Any && item.GetOs() != f.runtimeOs {
-			continue
+		if keepTag {
+			tagMatchedItems = append(tagMatchedItems, item)
 		}
 
-		if item.GetArch() != archtypes.Any && item.GetArch() != f.runtimeArch {
-			continue
-		}
-
-		compatibleItems = append(compatibleItems, item)
-
-		if len(item.GetRequiredTags()) > 0 {
+		if keepRequiredTag {
 			requiredTagMatchedItems = append(requiredTagMatchedItems, item)
 		}
-
-		if len(f.runtimeTags) > 0 &&
-			!containsAtLeastOneTag(f.runtimeTags, item.GetTags()) {
-			continue
-		}
-
-		tagMatchedItems = append(tagMatchedItems, item)
-	}
-
-	if len(compatibleItems) <= 0 {
-		return foundItem, false, nil
 	}
 
 	if len(requiredTagMatchedItems) > 0 {
@@ -151,7 +149,77 @@ func filter[T Filterable, E ~[]T](
 		return tagMatchedItems[0], true, nil
 	}
 
-	return compatibleItems[0], true, nil
+	if len(compatibleItems) > 0 {
+		return compatibleItems[0], true, nil
+	}
+
+	return foundItem, false, nil
+}
+
+func shouldKeep[T Filterable](
+	f *filterer,
+	item T,
+	restrictive bool,
+) (keepCompatible, keepTag, keepRequiredTag bool) {
+	compatible,
+		hasTags,
+		tagMatch,
+		hasRequiredTags,
+		requiredTagMatch,
+		hasRuntimeTags := getMatches(f, item)
+
+	if !compatible {
+		return false, false, false
+	}
+
+	if hasRequiredTags && !requiredTagMatch {
+		return false, false, false
+	}
+
+	keepCompatible = !restrictive
+	keepTag = !hasRuntimeTags || (hasTags && tagMatch)
+	keepRequiredTag = hasRequiredTags && requiredTagMatch
+
+	return keepCompatible, keepTag, keepRequiredTag
+}
+
+func getMatches[T Filterable](
+	f *filterer,
+	item T,
+) (
+	compatible,
+	hasTags,
+	tagMatch,
+	hasRequiredTags,
+	requiredTagMatch,
+	hasRuntimeTags bool,
+) {
+	return isCompatible(f, item),
+		len(item.GetTags()) > 0,
+		isTagMatch(f, item),
+		len(item.GetRequiredTags()) > 0,
+		isRequiredTagMatch(f, item),
+		len(f.runtimeTags) > 0
+}
+
+func isCompatible[T Filterable](f *filterer, item T) bool {
+	if item.GetOs() != ostypes.Any && item.GetOs() != f.runtimeOs {
+		return false
+	}
+
+	if item.GetArch() != archtypes.Any && item.GetArch() != f.runtimeArch {
+		return false
+	}
+
+	return true
+}
+
+func isTagMatch[T Filterable](f *filterer, item T) bool {
+	return containsAtLeastOneTag(f.runtimeTags, item.GetTags())
+}
+
+func isRequiredTagMatch[T Filterable](f *filterer, item T) bool {
+	return containsAllTags(f.runtimeTags, item.GetRequiredTags())
 }
 
 func containsAtLeastOneTag(runtimeTags, tags map[string]bool) bool {
