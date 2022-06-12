6cf566545015565472b368e28729cdb4b96f1428
Chris Jones <chris@Chriss-MacBook-Air.local>
2022-06-12 07:23:06 -0700

    add setup setuper runner

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/yo3jones/yconfig/setup"
@@ -13,38 +11,18 @@ var setupCmd = &cobra.Command{
 	Short: "execute setup steps",
 	Long:  "execute setup steps",
 	Run: func(_ *cobra.Command, _ []string) {
-		var (
-			s       *setup.Setup
-			pms     []setup.PackageManager
-			scripts []setup.Script
-			err     error
-		)
-
-		config := viper.Get("setup")
-		if s, err = setup.Parse(&config); err != nil {
-			panic(err)
-		}
-
-		s.Print()
-
-		pmsConfig := viper.Get("packageManagers")
-		if pms, err = setup.ParsePackageManagers(&pmsConfig); err != nil {
-			panic(err)
-		}
-
-		fmt.Println("packageManagers")
-		for _, pm := range pms {
-			pm.Print()
-		}
+		var err error
 
 		scriptsConfig := viper.Get("scripts")
-		if scripts, err = setup.ParseScripts(&scriptsConfig); err != nil {
-			panic(err)
-		}
+		packageManagersConfig := viper.Get("packageManagers")
+		config := viper.Get("setup")
 
-		fmt.Println("scripts")
-		for _, script := range scripts {
-			script.Print()
+		if err = setup.New().
+			ScriptsConfig(&scriptsConfig).
+			PackageManagersConfig(&packageManagersConfig).
+			Config(&config).
+			Setup(); err != nil {
+			panic(err)
 		}
 	},
 }
